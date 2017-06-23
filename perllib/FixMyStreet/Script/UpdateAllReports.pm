45e6a0af7644900b62924dac271f4958c924d4ca
Dave Arter <davea@mysociety.org>
2017-06-23 12:30:21 +0100

    squash! Include area stats in all-reports.json
    This commit improves the loop efficiency and uses a `--areas` flag instead
    of a general.yml config key for including areas.

--- perllib/FixMyStreet/Script/UpdateAllReports.pm
+++ perllib/FixMyStreet/Script/UpdateAllReports.pm
@@ -21,6 +21,8 @@ if ( FixMyStreet->config('BASE_URL') =~ /zurich|zueri/ ) {
 }
 
 sub generate {
+    my $include_areas = shift;
+
     my $problems = FixMyStreet::DB->resultset('Problem')->search(
         {
             state => [ FixMyStreet::DB::Result::Problem->visible_states() ],
@@ -42,22 +44,20 @@ sub generate {
         my @bodies;
         my @areas;
         my $cobrand = $problem{cobrand};
+        my $duration_str = ( $problem{duration} > 2 * $fourweeks ) ? 'old' : 'new';
+        my $type = ( $problem{duration} > 2 * $fourweeks )
+            ? 'unknown'
+            : ($problem{age} > $fourweeks ? 'older' : 'new');
+        my $problem_fixed =
+               FixMyStreet::DB::Result::Problem->fixed_states()->{$problem{state}}
+            || FixMyStreet::DB::Result::Problem->closed_states()->{$problem{state}};
 
-        if ( !$problem{bodies_str} ) {
-            # Problem was not sent to any bodies, add to all areas
-            @bodies = grep { $_ } split( /,/, $problem{areas} );
-            $problem{bodies} = 0;
-        } else {
-            # Add to bodies it was sent to
-            @bodies = split( /,/, $problem{bodies_str} );
-            $problem{bodies} = scalar @bodies;
-        }
+        # Add to bodies it was sent to
+        @bodies = split( /,/, $problem{bodies_str} );
+        $problem{bodies} = scalar @bodies;
+
         foreach my $body ( @bodies ) {
-            my $duration_str = ( $problem{duration} > 2 * $fourweeks ) ? 'old' : 'new';
-            my $type = ( $problem{duration} > 2 * $fourweeks )
-                ? 'unknown'
-                : ($problem{age} > $fourweeks ? 'older' : 'new');
-            if (FixMyStreet::DB::Result::Problem->fixed_states()->{$problem{state}} || FixMyStreet::DB::Result::Problem->closed_states()->{$problem{state}}) {
+            if ( $problem_fixed ) {
                 # Fixed problems are either old or new
                 $fixed{$body}{$duration_str}++;
                 $fixed{$cobrand}{$body}{$duration_str}++;
@@ -68,14 +68,10 @@ sub generate {
             }
         }
 
-        if ( FixMyStreet->config('AREAS_IN_ALLREPORTS_JSON') ) {
+        if ( $include_areas ) {
             @areas = grep { $_ } split( /,/, $problem{areas} );
             foreach my $area ( @areas ) {
-                my $duration_str = ( $problem{duration} > 2 * $fourweeks ) ? 'old' : 'new';
-                my $type = ( $problem{duration} > 2 * $fourweeks )
-                    ? 'unknown'
-                    : ($problem{age} > $fourweeks ? 'older' : 'new');
-                if (FixMyStreet::DB::Result::Problem->fixed_states()->{$problem{state}} || FixMyStreet::DB::Result::Problem->closed_states()->{$problem{state}}) {
+                if ( $problem_fixed ) {
                     $fixed{areas}{$area}{$duration_str}++;
                 } else {
                     $open{areas}{$area}{$type}++;
